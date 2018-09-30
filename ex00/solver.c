52eb8b30aa99e14f0abc0272577a416c71f498ce
Simon Moisselin <smoissel@e1z1r4p37.42.us.org>
2018-09-29 20:41:51 -0700

    solver beta version

--- ex00/solver.c
+++ ex00/solver.c
@@ -6,7 +6,7 @@
 /*   By: smoissel <smoissel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/29 11:19:58 by smoissel          #+#    #+#             */
-/*   Updated: 2018/09/29 20:13:10 by smoissel         ###   ########.fr       */
+/*   Updated: 2018/09/29 20:39:09 by smoissel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 int 	solver(struct Sudoku S)
 {	
-	int 	count_x;
 	int 	y;
 	int 	x;
 	int 	test_value;
@@ -22,28 +21,26 @@ int 	solver(struct Sudoku S)
 
 	x = 0;
 	y = 0;
-	test_value = 0;
+	test_value = 1;
+
+	if (!check(S))
+		return (0);
 	while (x <= 9)
 	{
 		while (y <= 9)
 		{
 			if (S.point[x][y] == 0 )
 			{	
-				while (++test_value != 9)
-				{	
-					while(!check(S, x, y, test_value))
-					test_value++;
+				while (test_value != 9)
+				{
 					S.point[x][y] = test_value;
-					solver(S);
-					{
-
-					}
+					result += solver(S);
 				}
+				test_value++;
 			}
 		}
 	}
-	/* Should print the final sudoku and return the number of possible solutions. */
-	return (0);
+	return (result);
 }
 
 int 	is_valide(struct Sudoku S)
